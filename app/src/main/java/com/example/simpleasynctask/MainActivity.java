3e85ebf38b0e5835a05ac0bdc869839ffc184498
GeorgeMacLennan <georg,ma6775@vusd.us>
2021-01-22 16:30:47 -0800

    Initial commit

---
+++ app/src/main/java/com/example/simpleasynctask/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.simpleasynctask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+    private static final String TEXT_STATE = "currentText";
+    private ProgressBar mProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.rtgw);
+        mProgressBar = findViewById(R.id.progressBar);
+
+        if (savedInstanceState!=null) {
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+        }
+    }
+
+    public void startTask(View v) {
+        mTextView.setText(R.string.sleeping);
+
+        new SimpleAsyncTask(mTextView, mProgressBar).execute();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString(TEXT_STATE,mTextView.getText().toString());
+    }
+
+}
